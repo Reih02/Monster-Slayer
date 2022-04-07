311436787d28fbcb8afdb8537a4dfd9359f05cdd
OrionLynch <102627856+OrionLynch@users.noreply.github.com>
2022-04-08 11:46:53 +1200

    Added comments to purchasable.java

--- src/main/Purchasable.java
+++ src/main/Purchasable.java
@@ -1,9 +1,25 @@
 package main;
 
+/**
+ * Purchasable interface
+ * This interface means that the classes that implement it must have the methods in purchasable.
+ * The classes that implement purchasable are Monster and item.
+ * @author Orion Lynch & Reilly Haskins.
+ *
+ */
+
 public interface Purchasable {
+	
+	/**
+	 * This method returns the purchase price where it is implemented.
+	 * @return The price to purchase the purchasable.
+	 */
 	public int getBuyPrice();
+	
+	/**
+	 * This method returns the sell price where it is implemented.
+	 * @return The price to sell the purchasable.
+	 */
 	public int getSellPrice();
 	
-	
-
 }
