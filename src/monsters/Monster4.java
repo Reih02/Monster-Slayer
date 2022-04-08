1e3826319c0f173490132b315541e12f9a0f8c42
OrionLynch <102627856+OrionLynch@users.noreply.github.com>
2022-04-08 12:10:26 +1200

    Update and rename Monster4.java to Volibear.java

--- src/monsters/Monster4.java
+++
@@ -1,7 +0,0 @@
-package monsters;
-
-import main.Monster;
-
-public class Monster4 extends Monster {
-
-}
---
+++ src/monsters/Volibear.java
@@ -0,0 +1,18 @@
+package monsters;
+
+import main.Monster;
+
+/**
+ * Volibear class that extends the super class Monster.
+ * @author Orion Lynch & Reilly Haskins.
+ */
+public class Volibear extends Monster {
+	
+	/**
+	 * Volibear constructor that calls the super constructor with default values.
+	 */
+	public Volibear() {
+		super(150, "Volibear", 10, 20);
+	}
+
+}
