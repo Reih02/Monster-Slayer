bb9b06066be8d03dc1edd9835b4c3c0a229c8dea
OrionLynch <102627856+OrionLynch@users.noreply.github.com>
2022-04-08 12:08:53 +1200

    Update and rename Monster1.java to MasterYi.java

---
+++ src/monsters/MasterYi.java
@@ -0,0 +1,18 @@
+package monsters;
+
+import main.Monster;
+
+/**
+ * MasterYi class that extends the super class Monster.
+ * @author Orion Lynch & Reilly Haskins.
+ */
+public class MasterYi extends Monster {
+	
+	/**
+	 * MasterYi constructor that calls the super constructor with default values.
+	 */
+	public MasterYi() {
+		super(100, "MasterYi", 20, 20);
+	}
+
+}
