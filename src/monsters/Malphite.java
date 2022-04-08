afbfb1bfa984a6958c22a729773663e209381260
OrionLynch <102627856+OrionLynch@users.noreply.github.com>
2022-04-08 12:09:22 +1200

    Update and rename Monster2.java to Malphite.java

---
+++ src/monsters/Malphite.java
@@ -0,0 +1,18 @@
+package monsters;
+
+import main.Monster;
+
+/**
+ * Malphite class that extends the super class Monster.
+ * @author Orion Lynch & Reilly Haskins.
+ */
+public class Malphite extends Monster {
+	
+	/**
+	 * Malphite constructor that calls the super constructor with default values.
+	 */
+	public Malphite() {
+		super(120, "Malphite", 15, 25);
+	}
+
+}
