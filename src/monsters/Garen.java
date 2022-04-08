184e756581bd5db31a71bece5f7373938c5fb30a
OrionLynch <102627856+OrionLynch@users.noreply.github.com>
2022-04-08 12:11:10 +1200

    Update and rename Monster6.java to Garen.java

---
+++ src/monsters/Garen.java
@@ -0,0 +1,18 @@
+package monsters;
+
+import main.Monster;
+
+/**
+ * Garen class that extends the super class Monster.
+ * @author Orion Lynch & Reilly Haskins.
+ */
+public class Garen extends Monster {
+	
+	/**
+	 * Garen constructor that calls the super constructor with default values.
+	 */
+	public Garen() {
+		super(90, "Garen", 25, 15);
+	}
+
+}
--- src/monsters/Monster6.java
+++
@@ -1,7 +0,0 @@
-package monsters;
-
-import main.Monster;
-
-public class Monster6 extends Monster {
-
-}
