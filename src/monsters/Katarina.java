3394cbc370df5c333b8710597a2c2955139763d5
OrionLynch <102627856+OrionLynch@users.noreply.github.com>
2022-04-08 12:09:48 +1200

    Update and rename Monster3.java to Katarina.java

---
+++ src/monsters/Katarina.java
@@ -0,0 +1,18 @@
+package monsters;
+
+import main.Monster;
+
+/**
+ * Katarina class that extends the super class Monster.
+ * @author Orion Lynch & Reilly Haskins.
+ */
+public class Katarina extends Monster {
+	
+	/**
+	 * Katarina constructor that calls the super constructor with default values.
+	 */
+	public Katarina() {
+		super(80, "Katarina", 30, 15);
+	}
+
+}
--- src/monsters/Monster3.java
+++
@@ -1,7 +0,0 @@
-package monsters;
-
-import main.Monster;
-
-public class Monster3 extends Monster {
-
-}
