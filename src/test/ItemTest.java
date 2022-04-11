3469f0befd675addd34a2490148be902b0939e8f
Reih02 <reillyhaskins@gmail.com>
2022-04-11 20:56:45 +1200

    Made test file for Item class

---
+++ src/test/ItemTest.java
@@ -0,0 +1,91 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.Item;
+import items.*;
+
+/*
+ * Test for Item class
+ * @author Reilly Haskins and Orion Lynch
+ * */
+class ItemTest {
+
+	/*
+	 * Initialising objects for both potion and food types to test
+	 * */
+	private Item testApple;
+	private Item testCarrot;
+	private Item testSteak;
+	private Item testHealthPotion;
+	private Item testStrengthPotion;
+	
+	/*
+	 * Creates potion and food items for each test
+	 * */
+	@BeforeEach
+	public void setUp() throws Exception {
+		testApple = new Apple(7, 5, "Apple", 7);
+		testCarrot = new Carrot(5, 3, "Carrot", 5);
+		testSteak = new Steak(10, 7, "Steak", 15);
+		testHealthPotion = new HealthPotion(25, 20, "Health Potion", 25);
+		testStrengthPotion = new StrengthPotion(25, 20, "Strength Potion", 25);
+	}
+
+	/*
+	 * Checks buy prices correct for potion and food
+	 * */
+	@Test
+	final void testGetBuyPrice() {
+		assertEquals(7, testApple.getBuyPrice());
+		assertEquals(5, testCarrot.getBuyPrice());
+		assertEquals(10, testSteak.getBuyPrice());
+		assertEquals(25, testHealthPotion.getBuyPrice());
+		assertEquals(25, testStrengthPotion.getBuyPrice());
+	}
+
+	/*
+	 * Checks sell prices correct for potion and food
+	 * */
+	@Test
+	final void testGetSellPrice() {
+		assertEquals(5, testApple.getSellPrice());
+		assertEquals(3, testCarrot.getSellPrice());
+		assertEquals(7, testSteak.getSellPrice());
+		assertEquals(20, testHealthPotion.getSellPrice());
+		assertEquals(20, testStrengthPotion.getSellPrice());
+	}
+
+	/*
+	 * Checks name assigned correctly
+	 * */
+	@Test
+	final void testGetName() {
+		assertEquals("Apple", testApple.getName());
+		assertEquals("Carrot", testCarrot.getName());
+		assertEquals("Steak", testSteak.getName());
+		assertEquals("Health Potion", testHealthPotion.getName());
+		assertEquals("Strength Potion", testStrengthPotion.getName());
+	}
+
+	
+	/*
+	 * Checks bonus value assigned correctly
+	 * */
+	@Test
+	final void testGetBonusValue() {
+		assertEquals(7, testApple.getBonusValue());
+		assertEquals(5, testCarrot.getBonusValue());
+		assertEquals(15, testSteak.getBonusValue());
+		assertEquals(25, testHealthPotion.getBonusValue());
+		assertEquals(25, testStrengthPotion.getBonusValue());
+	}
+
+}
