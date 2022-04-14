e1b82b3cc7be6fb03e7ce8f55a261db9c8066a7e
Reih02 <reillyhaskins@gmail.com>
2022-04-14 15:19:07 +1200

    Made test class for Shop

---
+++ src/test/ShopTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import items.Apple;
+import main.Shop;
+
+class ShopTest {
+
+	private Shop testShop;
+	private Apple apple = new Apple();
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		testShop = new Shop();
+	}
+
+	@Test
+	void testGetPurchasableList() {
+		assertEquals(5, testShop.getPurchasableList().size());
+	}
+
+	@Test
+	void testBuyPurchasable() {
+		assertEquals(apple.getClass(), testShop.buyPurchasable(0).getClass());
+	}
+
+	@Test
+	void testSellPurchasable() {
+		assertEquals(apple.getClass(), testShop.buyPurchasable(0).getClass());
+	}
+
+}
