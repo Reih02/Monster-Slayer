7e19594a210b9fd76da6620f9025991faf33657f
Reih02 <reillyhaskins@gmail.com>
2022-04-14 13:30:56 +1200

    Implemented Shop class

--- src/main/Shop.java
+++ src/main/Shop.java
@@ -1,5 +1,24 @@
 package main;
 
-public class Shop {
+import java.util.ArrayList;
 
+public class Shop {
+	
+	private ArrayList<Purchasable> purchasableList;
+	
+	public Shop() {
+		purchasableList = new ArrayList<Purchasable>();
+	}
+	
+	public ArrayList<Purchasable> getPurchasableList(){
+		return purchasableList;
+	}
+	
+	public Purchasable buyPurchasable(int index) {
+		return purchasableList.get(index);
+	}
+	
+	public Purchasable sellPurchasable(int index) {
+		return purchasableList.get(index);
+	}
 }
