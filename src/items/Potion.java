1a6b4990495bb9d614c7f02b95815953673fbd1f
OrionLynch <102627856+OrionLynch@users.noreply.github.com>
2022-04-26 15:49:05 +1200

    Update Potion.java

--- src/items/Potion.java
+++ src/items/Potion.java
@@ -10,7 +10,7 @@ public class Potion extends Item {
 	/*
 	 * Potion constructor, passes values into superclass (Item)
 	 * */
-	public Potion(int buyPrice, int sellValue, String name, int bonus) {
-		super(buyPrice, sellValue, name, bonus);
+	public Potion(int buyPrice, int sellValue, String name, int bonus, String difficulty) {
+		super(buyPrice, sellValue, name, bonus, difficulty);
 	}
 }
