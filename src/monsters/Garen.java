eeaea442f740e9a332f85b3a8c1acfa8c7ddf1ff
OrionLynch <102627856+OrionLynch@users.noreply.github.com>
2022-04-26 15:45:52 +1200

    Update Garen.java

--- src/monsters/Garen.java
+++ src/monsters/Garen.java
@@ -10,9 +10,10 @@ public class Garen extends Monster {
 	
 	/**
 	 * Garen constructor that calls the super constructor with default values.
+	 * @param difficulty the Difficulty setting that changes some of the monsters values.
 	 */
-	public Garen() {
-		super(90, "Garen", 25, 15);
+	public Garen(String difficulty) {
+		super(90, "Garen", 25, 15, difficulty);
 	}
 
 }
